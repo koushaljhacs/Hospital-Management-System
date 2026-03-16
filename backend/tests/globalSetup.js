95e8e89071d8b772a684c552bdb0cc8c0ba0708a
koushaljhacs <koushaljha.cs@gmail.com>
2026-03-16 12:40:47 +0530

    chore: remove test files from main
    - Remove test files from production branch
    - Keep main branch clean for production
    
    Team: OctNov
    Lead: Koushal Jha
    Date: 2026-03-16

--- backend/tests/globalSetup.js
+++
@@ -1,9 +0,0 @@
-
-const dotenv = require('dotenv');
-const path = require('path');
-const db = require('../src/config/database');
-
-module.exports = async () => {
-    dotenv.config({ path: path.resolve(__dirname, '../.env') });
-    await db.connectWithRetry();
-};
